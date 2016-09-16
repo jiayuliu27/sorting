eb8cfae2bd646222fd15c34b92af0786ce38b1fb
jiayuliu27 <jiayuliu@umich.edu>
2016-09-16 13:52:57 -0400

    update quickSort

--- quickSort.js
+++ quickSort.js
@@ -1,44 +1,31 @@
-function partition(arr, left, right, pivot) {
-	while(left <= right) {
-		console.log(left, right, "while partition");
-		while(arr[left] < pivot) {
-			++left;
-		}
-		while(arr[right] > pivot) {
-			--right;
-		}
-		console.log(left, right);
-		if(arr[left] > pivot && arr[right] < pivot) {
-			console(arr[left], arr[right]);
-			var tmp = arr[left];
-			arr[left++] = arr[right];
-			arr[right--] = tmp;
-			// arr[left], arr[right] = arr[right--], arr[left++];
-			console.log("hello", arr[left], arr[right]);
-		} 
-	}
-	return left;
-
-}
-
-function quickSort(arr, left = 0, right = -1, comp = less) {
-	
-	if(right === -1) {
-		right = arr.length - 1;
-	}
-
-	if(right <= left) {
-		return arr;
-	}
-	// middle elt as pivot
-	var pivot = Math.floor((right - left) / 2); //Math.floor(Math.random * (arr.length - 1));
-	var part = partition(0, arr.length, pivot);
-	console.log(part, pivot, arr);
-	if(left < part - 1) {
-		//quickSort(arr, left, part - 1);
-	}
-	if(part < right) {
-		// quickSort(arr, part, right);
-	}
-
+function quickSort(arr, left = 0, right = null) {
+      if(!right) {
+            right = arr.length - 1;
+      }
+      var i = left, j = right;
+      var tmp;
+      var pivot = arr[Math.floor((left + right) / 2)];
+      
+      /* partition */
+      while (i <= j) {
+            while (arr[i] < pivot)
+                  i++;
+            while (arr[j] > pivot)
+                  j--;
+            if (i <= j) {
+                  tmp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = tmp;
+                  i++;
+                  j--;
+            }
+      };
+ 
+      /* recursion */
+      if (left < j)
+            quickSort(arr, left, j);
+      if (i < right)
+            quickSort(arr, i, right);
+      
+      return arr;
 }
